ce7f030f81174277d6dd09c4502535d5e2347c7d
Talha Habib_4 <talhahabib956@gmail.com>
2024-09-30 12:08:46 +0500

    Modifications

--- javascript/index.js
+++ javascript/index.js
@@ -8,22 +8,24 @@ function downloadResume() {
 }
 
 
-document.onkeydown = function (e) {
-    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.key === 'U')) {
-        e.preventDefault();
-        return false;
-    }
-};
+// document.onkeydown = function (e) {
+//     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.key === 'U')) {
+//         e.preventDefault();
+//         return false;
+//     }
+// };
 
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-});
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+// });
 
-// Prevent Mobile Access
-function isMobile() {
-    return /Mobi|Android/i.test(navigator.userAgent);
-}
+// // Prevent Mobile Access
+// function isMobile() {
+//     return /Mobi|Android/i.test(navigator.userAgent);
+// }
 
-if (isMobile()) {
-    window.location.href = 'https://example.com/not-supported.html'; // Redirect to a not supported page
-}
+// if (isMobile()) {
+//     window.location.href = 'https://example.com/not-supported.html'; // Redirect to a not supported page
+// }
+
+
